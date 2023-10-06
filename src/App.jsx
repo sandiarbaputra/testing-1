a3daccc3320261e3ea3bb54792e0f8f48d89798f
sandiarbaputra <sandiarba09@gmail.com>
2023-10-06 15:28:46 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-sky-500 flex text-white">
+      <h1 className="m-auto text-7xl font-bold">Sandi Arba Putra</h1>
+    </div>
+  );
+}
+
+export default App;
